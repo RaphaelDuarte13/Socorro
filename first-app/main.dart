00cda687669ce80730b74546eb446e88953593da
energeticpants7 <118300043+energeticpants7@users.noreply.github.com>
2025-09-06 19:52:48 +0100

    teste

--- first-app/main.dart
+++ first-app/main.dart
@@ -3,10 +3,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
+"
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -120,3 +119,6 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  
