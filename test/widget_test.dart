5b76f3a2c3d9c12ad25f651c94e97e4eff63a30d
Energeticpants7 <albert123renan@gmail.com>
2025-09-06 22:49:38 +0100

    teste

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import '../lib/main.dart';
+import 'package:projeto11/main.dart'; // ajuste para o nome do seu projeto
 
 void main() {
+ 
+
 }
